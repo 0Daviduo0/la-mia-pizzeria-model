a9911211ca452f646d04200e88015e43d8b22643
Daviduo <davide0400@gmail.com>
2023-05-03 17:52:02 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Descr { get; set; }
+        public string Img { get; set; }
+        public decimal Price { get; set; }
+        public int Id { get; set; }
+
+        public Pizza(string nome, string descr, string img, decimal price, int id)
+        {
+            Nome = nome;
+            Descr = descr;
+            Img = img;
+            Price = price;
+            Id = id;
+        }
+    }
+}
